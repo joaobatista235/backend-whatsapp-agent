1133d45e0bc237e52872d5a195f502ad59a4e91e
João Batista <62124351+joaobatista235@users.noreply.github.com>
2024-11-30 20:05:37 -0300

    Delete src directory

--- src/config/firebaseConfig.js
+++
@@ -1,7 +0,0 @@
-import admin from "firebase-admin";
-
-admin.initializeApp({
-  credential: admin.credential.cert("./src/config/serviceAccountKey.json"),
-});
-
-export const db = admin.firestore();
--- src/router.js
+++
@@ -1,7 +0,0 @@
-import agentRoutes from "./routes/agent.js";
-import gloogleRoutes from "./routes/gloogle.js";
-
-export default (app) => {
-  app.use("/agent", agentRoutes);
-  app.use("/", gloogleRoutes);
-};
